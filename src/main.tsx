040f731d64a7cb5641665bbab54ebe5594fe4256
Andrey Akushevich <andreibobryisk@gmail.com>
2025-12-06 13:29:00 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "7/src/index.css";
 import App from "7/src/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
