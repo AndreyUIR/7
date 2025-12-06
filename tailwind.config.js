5948a0e1ecfa61e45b80951ebe5c89d180ac9d6b
Andrey Akushevich <andreibobryisk@gmail.com>
2025-12-06 11:22:22 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "7/index.html",
+    "7/src/components/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
