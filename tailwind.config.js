23f350f63df5e89bb2781460ee15ca3dae9d5bcf
Andrey Akushevich <andreibobryisk@gmail.com>
2025-12-06 14:04:55 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "7/index.html",
-    "7/src/components/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
